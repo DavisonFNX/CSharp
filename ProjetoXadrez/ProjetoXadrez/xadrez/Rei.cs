4e3a9ab287f12edd7f8bb0b83be836f84d1e6421
Davison Nascimento <davisonfranciscodonascimento@Mac-mini-de-Davison.local>
2023-02-14 14:21:02 -0300

    Adicionando o projeto ProEvento

---
+++ ProjetoXadrez/ProjetoXadrez/xadrez/Rei.cs
@@ -0,0 +1,22 @@
+﻿using tabuleiro;
+namespace xadrez
+{fads
+	{
+		public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+		{3wq
+            dfaesd q`2
+
+                a
+
+		}
+        96269626
+        9626
+        96269626ads
+
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}
+
